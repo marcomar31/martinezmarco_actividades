4d8096743fd75f564b3f1a0dc6e238913abf9b26
mmartinez <marcomartinezfpdam@gmail.com>
2023-09-27 12:30:57 +0200

    Commit 3: Añadidos "InputTexts" a LoginView.dart

--- lib/OnBoarding/LoginView.dart
+++ lib/OnBoarding/LoginView.dart
@@ -10,10 +10,31 @@ class LoginView extends StatelessWidget {
     _context = context;
 
     Column column = new Column(children: [
-      Text("Login")
+      //USUARIO
+      Padding(padding: EdgeInsets.symmetric(horizontal: 50, vertical: 16),
+        child: TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Escriba su usuario',
+          ),
+        ),
+      ),
+
+      //CONTRASEÑA
+      Padding(padding: EdgeInsets.symmetric(horizontal: 50, vertical: 16),
+        child: TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Escriba su contraseña',
+          ),
+          obscureText: true,
+        ),
+      )
     ],);
 
-    return column;
+    Scaffold scaffold = Scaffold(body: column,);
+
+    return scaffold;
   }
 
 }
