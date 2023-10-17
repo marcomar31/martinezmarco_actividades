252b30b126f1c92e41e3cd41cb0811b1841b9e9a
MartinezDelAlamoMarco <marco.martinez.profesional@gmail.com>
2023-10-17 19:50:40 +0200

    Commit 34: En HomeView.dart se recuperará la colección de "Posts" de Firebase Firestore y se mostrarán en una lista.

--- lib/Main/HomeView.dart
+++ lib/Main/HomeView.dart
@@ -1,20 +1,49 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
+import '../Custom/PostCellView.dart';
+import '../FirestoreObjects/FbPost.dart';
+
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  final List<FbPost> posts = [];
+
+  Widget? creadorDeItemLista(BuildContext context, int index) {
+    return PostCellView(sText: posts[index].titulo,
+        dFontSize: 20);
+  }
+
+  Widget creadorDeSeparadorLista(BuildContext context, int index) {
+    return Divider(color: Color.fromRGBO(37, 77, 152, 1.0), thickness: 2,);
+  }
+
+  @override
+  void initState() {
+    descargarPosts();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-        body: Column(
-          children: [
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Padding(padding: EdgeInsets.symmetric(horizontal: 50, vertical: 16), child:
-                Text('BIENVENIDO!!'),
-              ),
-            ]),
-          ],),
+      body:
+      Padding(padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16), child:
+        Center(child:
+          ListView.separated(
+            padding: EdgeInsets.all(80),
+            itemCount: posts.length,
+            itemBuilder: creadorDeItemLista,
+            separatorBuilder: creadorDeSeparadorLista,
+          ),
+        ),
+      ),
       appBar: AppBar(
         title: const Text("HOME"),
         centerTitle: true,
@@ -25,4 +54,17 @@ class HomeView extends StatelessWidget {
     );
   }
 
+  void descargarPosts() async {
+    CollectionReference<FbPost> reference = db
+        .collection("Posts")
+        .withConverter(fromFirestore: FbPost.fromFirestore,
+        toFirestore: (FbPost usuario, _) => usuario.toFirestore());
+
+    QuerySnapshot<FbPost> querySnap = await reference.get();
+    for (int i = 0; i < querySnap.docs.length; i++) {
+      setState(() {
+        posts.add(querySnap.docs[i].data());
+      });
+    }
+  }
 }
