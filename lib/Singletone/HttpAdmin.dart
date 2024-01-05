f28cd3dd597a77113f5f91d0d795f02f74c027e8
Marco Martínez <marcomar3104gaming@gmail.com>
2024-01-05 13:09:30 +0100

    Commit 59: Añadida api "open-meteo" a HttpAdmin, y añadido el método para obtener la temperatura según la latitud y la longitud que se pase por parámetro.

--- lib/Singletone/HttpAdmin.dart
+++ lib/Singletone/HttpAdmin.dart
@@ -1,3 +1,38 @@
-class HttpAdmin {
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+class HttpAdmin{
+
+
   HttpAdmin();
+
+
+  Future<double> pedirTemperaturasEn(double latitud, double longitud) async{
+    var url = Uri.https('api.open-meteo.com', '/v1/forecast',
+        { 'latitude': latitud.toString(),
+          'longitude': longitud.toString(),
+          'hourly': 'temperature_2m',
+        });
+
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var jsonResponse = convert.jsonDecode(response.body) as Map<String, dynamic>;
+      DateTime now = DateTime.now();
+      int hora = now.hour;
+
+      var jsonHourly = jsonResponse['hourly'];
+      var jsonTemperaturas=jsonHourly['temperature_2m'];
+      var jsonTemperatura0=jsonTemperaturas[hora];
+
+      return jsonTemperatura0;
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+      return 0;
+    }
+
+  }
+
+
+
 }
