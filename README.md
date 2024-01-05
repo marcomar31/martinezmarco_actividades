a5ba129511fb97f7c2b2122c796c1346a70e58e6
Marco Martínez <marcomar3104gaming@gmail.com>
2024-01-05 16:23:58 +0100

    Commit 65: Creada clase GeolocAdmin, para posteriormente poder gestionar la geolocalización.

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Actividad 2. Marco Martínez del Álamo (Commits 21 - 38).
 
 Actividad 3. Marco Martínez del Álamo (Commits 39 - 56).
 
-Actividad 4. Marco Martínez del Álamo (Commits 57 - 64).
+Actividad 4. Marco Martínez del Álamo (Commits 57 - 65).
---
+++ lib/Singletone/GeolocAdmin.dart
@@ -0,0 +1,44 @@
+
+import 'dart:async';
+
+import 'package:geolocator/geolocator.dart';
+
+class GeolocAdmin{
+
+  late StreamSubscription<Position> positionStream;
+
+  Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+
+  void registrarCambiosLoc(Function(Position? position) funCambioPos){
+    final LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 0,
+    );
+
+    positionStream = Geolocator.getPositionStream(locationSettings: locationSettings).listen(funCambioPos);
+  }
+
+}
